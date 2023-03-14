97b55a2ea646cb5dae5d6ecd2eeb6a342ad0dfd4
Vajay Attila <vajay.attila@gmail.com>
2023-03-14 17:57:48 +0100

    Update CommandLineParser.h
    Add doxigen tags: @email, @git

--- CommandLineParser.h
+++ CommandLineParser.h
@@ -4,6 +4,8 @@
  * @brief The definition of the CommandLineParser class for handling command-line parameters.
  *
  * @author Attila Vajay
+ * @email vajay.attila@gmail.com
+ * @git https://github.com/vajayattila/CommandLineParser.git 
  * @date 2023.03.14.
  * @version 1.0
  * @license MIT License
@@ -20,6 +22,7 @@
  * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
+ * For more information, see the file LICENSE.
  */
 
 #ifndef COMMANDLINEPARSER_H_
